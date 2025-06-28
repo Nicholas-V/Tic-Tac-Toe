0084c266468e39b8a960844caf1349d5ac1c14c9
Nicholas-V <nickvasylyniuk@gmail.com>
2025-06-28 13:49:17 -0400

    add GameBoard Component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-export default function Player({initialName, symbol}) {
+export default function Player({initialName, symbol, isActive}) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
@@ -13,15 +13,15 @@ export default function Player({initialName, symbol}) {
     }
 
     let editablePlayerName = <span className="player-name">{playerName}</span>;
-    if(isEditing) {
-        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
     }
 
     return (
-        <li>
+        <li className={isActive ? "active" : "non-active" } data-testid={playerName.toLowerCase()}>
             <span className="player">
                 {editablePlayerName}
-            <span className="player-symbol">{symbol}</span>
+                <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
