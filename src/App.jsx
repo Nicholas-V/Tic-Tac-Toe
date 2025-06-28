eb5ab2faffb6cae0fb74b23f819d7e3e10ac7f13
Nicholas-V <nickvasylyniuk@gmail.com>
2025-06-28 17:32:45 -0400

    add handle player name

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,10 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+    const [players, setPlayers, setState] = useState({
+        X: "Player 1",
+        O: "Player 2"
+    });
     const [gameTurns, setGameTurns] = useState([]);
     const activePlayer = deriveActivePlayer(gameTurns);
 
@@ -38,7 +42,7 @@ function App() {
         const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
 
         if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
-            winner = firstSquareSymbol;
+            winner = players[firstSquareSymbol];
         }
     }
 
@@ -56,11 +60,20 @@ function App() {
         setGameTurns([]);
     }
 
+    function handlePlayerNameChange(symbol, newName){
+        setPlayers(prevPlayers => {
+            return {
+                ...prevPlayers,
+                [symbol]: newName
+            }
+        });
+    }
+
     return (
         <main>
             <div id="game-container">
                 <ol id="players" data-testid="players" className="highlight-player">
-                    <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
+                    <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} onChangeName={handlePlayerNameChange}/>
                     <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
                 </ol>
                 {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
